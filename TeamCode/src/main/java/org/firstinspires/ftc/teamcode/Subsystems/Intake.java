19218a6e2ed08e3fa6d4bd7cb11fdc38c2e81770
EmilMuzafarov <126209993+EmilMuzafarov@users.noreply.github.com>
2025-01-02 16:24:35 -0500

    Update Intake.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
@@ -1,4 +1,24 @@
-package org.firstinspires.ftc.teamcode.Subsystems;
+package Subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Intake {
+    private DcMotor intakeMotor;
+
+    // Constructor to initialize the intake motor
+    public Intake(HardwareMap hardwareMap) {
+        intakeMotor = hardwareMap.get(DcMotor.class, "intake_motor");
+        intakeMotor.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+    // Intake operation
+    public void runIntake(double power) {
+        intakeMotor.setPower(power);
+    }
+
+    // Stop intake motor
+    public void stop() {
+        intakeMotor.setPower(0);
+    }
 }
